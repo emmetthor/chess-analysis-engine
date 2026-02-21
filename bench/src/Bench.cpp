e3f5c5fd8772aaaec4794334df2175fe17c68aa0
emmetthor <emmetthor@gmail.com>
2026-02-21 08:16:05 +0800

    [CI-test-redo] 新增錯誤輸出報告，並在錯誤時 main return 1

--- bench/src/Bench.cpp
+++ bench/src/Bench.cpp
@@ -5,9 +5,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "========== Mate In One ==========\n";
-    testMateInOne(10000);
+    testResult res;
 
-    // std::cout << "========== Mate In Two ==========\n";
-    // testMateInTwo(10000);
+    std::cout << "========== Mate In One ==========\n";
+    res += testMateInOne(10000);
+
+    std::cout << "========== Mate In Two ==========\n";
+    res += testMateInTwo(10000);
+
+    std::cout << "========== Result ==========\n";
+    std::cout << "Failed test cases: " << res.failedTests << '/' << res.totalTests << '\n';
+    std::cout << "Accuracy: " << (res.failedTests * 100 / res.totalTests) << "%\n";
+
+    if (res.failedTests != 0) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
--- bench/src/Bench.h
+++ bench/src/Bench.h
@@ -1,6 +1,12 @@
 #pragma once
 
 struct testResult {
-    int totalTests;
-    int failedTests;
+    int totalTests = 0;
+    int failedTests = 0;
+
+    testResult & operator+=(const testResult& o) {
+        totalTests += o.totalTests;
+        failedTests += o.failedTests;
+        return *this;
+    }
 };
