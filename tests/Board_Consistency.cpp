a42594d1feefef4c9e87484264c145286bade814
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-03-29 21:02:58 +0800

    update `Board_Consistency` test

---
+++ tests/Board_Consistency.cpp
@@ -0,0 +1,63 @@
+#include "fen/FEN_Parser.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "board/Board.h"
+#include "move/Generate_Move.h"
+#include "move/Make_Move.h"
+#include "search/Zobrist.h"
+
+int boardConsistency(Board& board, int depth)
+{
+    if (depth <= 0)
+        return 1;
+
+    if (board.zobristKey != computeZobrist(board))
+    {
+        ENGINE_FATAL(DebugCategory::BOARD, "different zobrist");
+    }
+
+    for (int pIndex = 1; pIndex <= 12; pIndex++)
+    {
+        Piece p = static_cast<Piece>(pIndex);
+        for (int i = 0; i < board.getPieceCount(p); i++)
+        {
+            if (board.at(board.getPiecePos(p)[i]) != p)
+            {
+                ENGINE_FATAL(DebugCategory::BOARD, "different piece position");
+            }
+        }
+    }
+
+    int node = 0;
+
+    Move moves[256];
+    int nMoves = generateAllLegalMoves(board, moves);
+
+    for (int i = 0; i < nMoves; i++)
+    {
+        Move move = moves[i];
+        makeMove(board, move);
+        node += boardConsistency(board, depth - 1);
+        undoMove(board, move);
+    }
+
+    return node;
+}
+
+int main()
+{
+    std::vector<std::pair<std::string, int>> fenList = {
+        {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 8902}
+    };
+
+    for (int i = 0; i < (int)fenList.size(); i++)
+    {
+        auto [str, node] = fenList[i];
+        Board board = cinFenToBoard(str);
+        assert(boardConsistency(board, 3) == node);
+        std::cout << "test " << i + 1 << " completed with " << node << " nodes.\n";
+    }
+}
