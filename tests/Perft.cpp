52b586b435d9b11436ba46f4376813e4264379f9
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-04 17:36:16 +0800

    remove unused include

--- tests/Perft.cpp
+++ tests/Perft.cpp
@@ -1,9 +1,7 @@
 #include "../engine/include/board/Board.h"
 #include "../engine/include/fen/FEN_Parser.h"
 #include "../engine/include/move/Generate_Move.h"
-#include "debug.h"
 #include "move/Make_BitMove.h"
-#include "search/Zobrist.h"
 #include <iostream>
 #include <string>
 #include <vector>
