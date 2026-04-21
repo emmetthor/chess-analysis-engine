1dbe71a8fa293fb0f3aa9adebeeedcc2ab07b3f1
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-21 21:26:30 +0800

    Add check evasions

--- engine/include/search/Search_Variables.h
+++ engine/include/search/Search_Variables.h
@@ -2,7 +2,7 @@
 
 namespace SearchVarialble
 {
-constexpr int MAX_SEARCH_DEPTH = 128;
-constexpr int MAX_PLY = 128;
+constexpr int MAX_SEARCH_DEPTH = 20;
+constexpr int MAX_PLY = 20;
 constexpr int MAX_GAME_PLY = 2048;
 } // namespace SearchVarialble
