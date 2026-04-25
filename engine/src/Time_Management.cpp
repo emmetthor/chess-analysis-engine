8ea495d7a943a6897a132f18c2575975afd127cd
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-25 09:02:35 +0800

    Improve low time management

--- engine/src/Time_Management.cpp
+++ engine/src/Time_Management.cpp
@@ -1,8 +1,26 @@
 #include "Time_Management.h"
+#include <algorithm>
 
 int64_t timeManageCore(int64_t timeLeft, int64_t increment)
 {
-    return timeLeft / 30 + increment;
+    int64_t safeTime = std::max<int64_t>(0, timeLeft - MOVE_OVERHEAD);
+
+    // 0.2 seconds
+    if (safeTime <= 200)
+    {
+        return std::max<int64_t>(MIN_THINK_TIME, increment / 2);
+    }
+
+    // 1 seconds
+    else if (safeTime <= 1000)
+    {
+        return std::max<int64_t>(safeTime / 8, increment);
+    }
+
+    // normal
+    else {
+        return safeTime / 30 + increment * 3 / 4;
+    }
 }
 
 SearchLimits timeManager(const TimeManage& tm, Player player)
