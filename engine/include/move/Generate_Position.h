7118ecf50f6600094e29ff5f827cf5ba43efe5f1
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-27 00:57:01 +0800

    Reviewed Generate_Position.h

--- engine/include/move/Generate_Position.h
+++ engine/include/move/Generate_Position.h
@@ -2,6 +2,6 @@
 
 #include "board/Board.h"
 
-int generatePiecePosFromPos(const Board& board, const Position& pos, Piece p, Position* buffer);
+int generatePiecePosFromPos(const Board& board, const Position& pos, const Piece p, Position* buffer);
 
-int generatePieceCaptureFromPos(const Board& board, const Position& pos, Piece p, Position* buffer);
+int generatePieceCaptureFromPos(const Board& board, const Position& pos, const Piece p, Position* buffer);
