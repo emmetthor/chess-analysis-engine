346c98e6c702ad7e7952d3f4950e620924ccb163
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-29 00:41:33 +0800

    Reviewed Check.cpp

--- engine/src/board/Check.cpp
+++ engine/src/board/Check.cpp
@@ -5,19 +5,14 @@
 // Check whether the king of the player is attacked.
 bool isInCheck(const Board& board, const Player player)
 {
-    Piece king = makePiece(player, 'K');
-    int kingCount = board.getPieceCount(king);
+    const Piece king = makePiece(player, 'K');
+    const int kingCount = board.getPieceCount(king);
     const auto* posArray = board.getPiecePos(king);
-    for (int i = 0; i < kingCount; i++)
-    {
-        if (isSquareAttacked(board, posArray[i], opponent(player)))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    return false;
+
+    ENGINE_ASSERT(kingCount == 1);
+
+    if (isSquareAttacked(board, posArray[0], opponent(player)))
+        return true;
+    else
+        return false;
 }
