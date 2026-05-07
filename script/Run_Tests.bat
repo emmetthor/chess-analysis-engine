a05d7916db037e68842e0b63adffff7a4f78ffda
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-05-07 18:57:15 +0800

    Update Run_Tests.bat

--- script/Run_Tests.bat
+++ script/Run_Tests.bat
@@ -1,19 +1,31 @@
 @echo off
-echo Running Perft...
 
-python ./tests/perft/Perft_Test.py --engine build/Hynobius.exe --file tests/perft/Perft_Tests.txt --max-depth 4
+echo ===
+echo Running Board Consistency test...
+echo ===
+
+python tests\board_consistency.py --engine build/Hynobius.exe --file tests/board_consistency/openings_bc.txt --depth 2 --detail
+python tests\board_consistency.py --engine build/Hynobius.exe --file tests/board_consistency/middle_game_bc.txt --depth 2 --detail
+python tests\board_consistency.py --engine build/Hynobius.exe --file tests/board_consistency/end_game_bc.txt --depth 2 --detail
+python tests\board_consistency.py --engine build/Hynobius.exe --file tests/board_consistency/stress_bc.txt --depth 2 --detail
+
+echo ===
+echo Running Perft...
+echo ===
+
+python ./tests/Perft_Test.py --engine build/Hynobius.exe --file tests/perft/Perft_Tests.txt --max-depth 4
 
 echo ===
 echo Running Regression...
+echo ===
 
 python tests\regression_test.py --engine build/Hynobius.exe --file tests/regression/regressions.txt --detail
 
 echo ===
 echo Running Checkmate test...
+echo ===
+
+python tests\checkmate.py --engine build/Hynobius.exe --file tests/checkmate/Mate_In_One.txt --mate-depth 2 --detail
 
 python tests\checkmate.py --engine build/Hynobius.exe --file tests/checkmate/Mate_In_Two.txt --mate-depth 2 --detail
 
-echo ===
-echo Running Board Consistency test...
-
-python tests\board_consistency.py --engine build/Hynobius.exe --file tests/board_consistency/stress_bc.txt --depth 2 --detail
